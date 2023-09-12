9cbc4d404e78254167697dacf4aaac83301eda9a
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-09-12 02:33:11 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Generate a random number, determine its last digit,
+ *              and print a message based on the last digit.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void affichage(char t[]) {
+for (int i = 0; t[i] != '\0'; i++) 
+{
+putchar(t[i]);
+}
+putchar('\n');
+}
+int main(void) {
+char a[] = "_putchar";
+affichage(a);
+return (0);
+}
