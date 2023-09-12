c807479e1e98132c4512ca65784194f61840b0ae
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-09-12 06:32:55 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Displays a string followed by a newline
+ *
+ * Description: This function takes a string as input and displays
+ *              it character by character, followed by a newline.
+ * Return: None
+ */
+
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
