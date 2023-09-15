cfa1a2a1dff3ae8a683154e7429155612ad032d1
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-09-15 01:50:37 -0700

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_last_digit - Prints the last digit of a number
- * @n: The number to extract the last digit from
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check.
  *
- * Return: The value of the last digit
+ * Return: 1 and prints + if n is greater than zero,
+ *         0 and prints 0 if n is zero,
+ *        -1 and prints - if n is less than zero.
  */
-int print_last_digit(int n)
+int print_sign(int n)
 {
-int last_digit;
-
-if (n < 0)
-n = -n;
-
-last_digit = n % 10;
-putchar('0' + last_digit);
-
-return last_digit;
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+    {
+_putchar('-');
+return (-1);
+}
 }
