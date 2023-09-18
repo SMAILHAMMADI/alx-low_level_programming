a8b5e914dace599e301ee65cbd60f6caa2dc9539
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-09-18 16:43:51 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,13 +8,11 @@
 void puts2(char *str)
 {
 int i = 0;
-while (str[i] != '.')
 {
 while (str[i] != '\0')
 {
-putchar(str[i]);
+_putchar(str[i]);
 i += 2;
 }
-}
-putchar('\n');
+_putchar('\n');
 }
