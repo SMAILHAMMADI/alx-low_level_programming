17e68d19a6c0a6aaedcc437e3177a2308ec66a44
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-09-19 02:48:07 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * puts_half - Prints half of a string, followed by a new line.
@@ -10,7 +11,7 @@ void puts_half(char *str)
 int length;
 int i;
 int start;
-length = _strlen(str);
+length = strlen(str);
 if (length % 2 == 0)
 {
 start = length / 2;
