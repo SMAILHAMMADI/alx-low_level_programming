fbb99bca672272d21069865080dc4cdcf55a5031
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-09-21 12:57:03 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,11 +15,11 @@ char sep[] = " \t\n,;.!?\"(){}";
 if (str[0] >= 'a' && str[0] <= 'z')
 str[0] = str[0] - ('a' - 'A');
 for ( j = 0 ; j < strlen(sep) ; j++)
-{ 
+{
 for(i = 0 ; i < strlen(str)-1 ; i++)
-{ 
-if (str[i]==separator[j])
-{ 
+{
+if ((str[i+1] >= 'a' && str[i+1] <= 'z')&&(str[i]==sep[j]))
+{
 str[i+1] = str[i+1] - ('a' - 'A');
 }
 }
