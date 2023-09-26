70a952f19411d2a26c2cc5b6b40431b3273dec3a
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-09-26 11:36:44 -0700

    a

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,10 @@
 /**
- * string_toupper -all lowercars in a string to uppercase
+ * _memset - Fills memory with a constant byte
+ * @s: Pointer to the memory area to be filled
+ * @b: The character to fill the memory with
+ * @n: The number of bytes to be filled
  *
- * Return: A pointer to the resulting string
+ * Return: A pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
