5ac6268e6b7cc48f79c98a2a53e47edc9ea4b4d8
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-09-26 05:04:53 -0700

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,17 +7,14 @@
  *
  * Return: pointer to the first occurrence of c in s, or NULL if not found
  */
+
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+int i; 
+for (i = 0; s[i] != '\0'; i++)
 {
-if (*s == c)
-return (s);
-s++;
+if (s[i] == c)
+return &(s[i]);
 }
-
-if (c == '\0')
-return (s);
-
 return NULL;
 }
