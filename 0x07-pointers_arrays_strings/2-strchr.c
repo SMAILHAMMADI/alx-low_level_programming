00f3cb13dba769449e7d3241fd27fcea602ba27b
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-09-26 12:09:24 -0700

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,11 @@
 
 char *_strchr(char *s, char c)
 {
-int i; 
-for (i = 0; s[i] != '\0'; i++)
+int i;
+for(i = 0; s[i] >= '\0'; i++)
 {
-if ( c == '\0')
-return s;
-if (s[i] == c)
-return &(s[i]);
+if(s[i] == c)
+return (s+i);
 }
 return (NULL);
 }
