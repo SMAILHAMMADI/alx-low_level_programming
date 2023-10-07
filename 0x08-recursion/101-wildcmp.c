cdb761df76f8b0840707ec959c31eb0d797c51a1
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-10-07 07:13:01 -0700

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * wildcmp - Compares two strings and returns 1 if tidentical.
+ * wildcmp - Compares two strings and returns 1 if they can dentical.
  * @s1: The first string to compare.
- * @s2: The second string to compare, which can contain tracter '*'.
+ * @s2: The second string to compare, which can contain the sparacter '*'.
  *
  * Return: 1 if the strings are identical, otherwise 0.
  */
@@ -13,13 +13,7 @@ if (*s2 == '*')
 {
 if (*(s2 + 1) == '\0')
 return (1);
-while (*s1 != '\0')
-{
-if (wildcmp(s1, s2 + 1))
-return (1);
-s1++;
-}
-return (0);
+return ((wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)));
 }
 if (*s1 == *s2 || (*s1 != '\0' && *s2 == '?'))
 return (wildcmp(s1 + 1, s2 + 1));
