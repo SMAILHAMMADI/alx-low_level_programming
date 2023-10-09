d3f20e2be6ddf60192d6ec56996c621a2456a176
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-10-09 15:15:13 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,48 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - Calculates the minimum number of coin amount of money.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0 on success, 1 on error.
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+
+	printf("%d\n", result);
+	return (0);
 }
 
-int cents = atoi(argv[1]);
-
-if (cents < 0)
-{
-printf("0\n");
-return (0);
-}
-
-int coins = 0;
-
-while (cents > 0)
-{
-if (cents >= 25)
-cents -= 25;
-else if (cents >= 10)
-cents -= 10;
-else if (cents >= 5)
-cents -= 5;
-else if (cents >= 2)
-cents -= 2;
-else
-cents -= 1;
-
-coins++;
-}
-
-printf("%d\n", coins);
-
-return (0);
-}
